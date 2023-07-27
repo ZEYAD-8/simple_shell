bf5f4a56ae8d2f072010b50207326f7415868fc9
ZEYAD-8 <zeyadosama15@gmail.com>
2023-07-27 04:50:32 +0300

    Action functions

--- helper_2.c
+++ helper_2.c
@@ -32,7 +32,7 @@ int display_prompt(void)
 {
 	int flush_return, print_return;
 
-	print_return = printf(">> ");
+	print_return = printf("$ ");
 	flush_return = fflush(stdout);
 	if (flush_return == 0 && print_return == 2)
 		return (0);
